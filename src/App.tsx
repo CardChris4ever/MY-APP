b16ecf9fcb73af49ee95459b47a7371fa94b685a
Christian <cardchris4ever@gmail.com>
2025-10-02 15:45:18 -0600

    02102025

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
 import { useState } from "react";
 
-function App(){
-  const [count,setCount] = useState<number>(0);
-  const [nombre,setNombre] = useState<string>("");
-  
-  return (
-    <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"100vh",flexDirection:"column"}}>
-      <input value={nombre} placeholder = "Esbribe tu nombre" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNombre(e.target.value)} />
 
-      <h1>Hola {nombre||"Anonime"}</h1>
-      <h2>Contador: {count} {count > 5 && <h2> que buena onda tienes muchos </h2>  }</h2> 
-      <div>
-        <button onClick={()=>setCount(count+2)}>Incrementar</button>
-        <button onClick={()=>setCount(count-2)}>Decrementar</button>
-        <button onClick={()=>setCount(0)}>Resetear</button>
-      </div>
-    </div>
-  );
-}
-function App2(){
+function App(){
   const [tareas,setTareas ]= useState<string[]>(["Estudiar React","Estudiar TypeScript","Estudiar NodeJS"]);
   const [tarea,setTarea ]= useState<string>("");
 
@@ -29,7 +12,7 @@ function App2(){
         {tareas.map((t, index) => (
           <li key={index}>{t}
             <button onClick={() => setTareas (
-              tareas.filter((element,i) => element!== t )
+              tareas.filter((_,i) => i !== index )
               )}>X</button>
           </li>
         ))}
@@ -44,4 +27,4 @@ function App2(){
   );
 }
 
-export default App2;
+export default App;
